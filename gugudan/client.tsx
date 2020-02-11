f3cf04f0f13fd6825516d4301d6fd2ee6ed72e90
Johny Kim <johnyworld@naver.com>
2020-02-11 16:05:35 +0900

    Change folder name

---
+++ gugudan/client.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import App from './Components/App';
+
+ReactDOM.render(<App />, document.querySelector('#app'));
---
+++ gugudan/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    mode: 'development', // production
+    devtool: 'eval', // hidden-source-map
+    resolve: {
+        extensions: [ '.jsx', '.js', '.tsx', '.ts' ],
+    },
+    entry: {
+        app: './client'
+    },
+    module: {
+        rules: [{
+            test: /\.tsx?$/,
+            loader: 'awesome-typescript-loader',
+        }]
+    },
+    plugins: [],
+    output: {
+        filename: 'app.js',
+        path: path.join(__dirname, 'dist'),
+        publicPath: '/dist',
+    }
+}
